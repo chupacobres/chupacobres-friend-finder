edcbde17c4b68c8270736243e29ac1cb4a630cbf
juan uribe <juanpablouribe@mac.com>
2019-12-13 17:09:31 -0500

    friends.js added with 4 friends to match with module.exports

---
+++ app/data/friends.js
@@ -0,0 +1,84 @@
+
+var friends = [
+    {
+        "name": "Papi",
+        "photo": "https://am11.akamaized.net/med/cnt/uploads/2010/01/Screen-shot-2010-01-31-at-11.47.04-AM-e1264957975704.png",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "Donald Trump",
+        "photo": "http://baskino.me/uploads/images/2013/104/oqnx12.jpg",
+        "scores": [
+            5,
+            5,
+            5,
+            5,
+            3,
+            5,
+            5,
+            5,
+            5,
+            5
+        ]
+    },
+    {
+        "name": "Cata Lina",
+        "photo": "http://images.tmz.com/2016/09/15/0915-hatchet-face-kim-macguire-cry-baby-5.jpg",
+        "scores": [
+            5,
+            5,
+            5,
+            5,
+            3,
+            3,
+            3,
+            5,
+            5,
+            5
+        ]
+    },
+    {
+        "name": "Satan",
+        "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBCzJN2jYwe6IDoShgz21y5fvOVg-EbvQTfM1VzKnv6QvejX-E",
+        "scores": [
+            3,
+            5,
+            3,
+            5,
+            3,
+            5,
+            3,
+            5,
+            3,
+            5
+        ]
+    },
+    {
+        "name": "Lovee Mi",
+        "photo": "https://ace_hotel_dev.s3.amazonaws.com/images/queen_of_the_damned.jpg",
+        "scores": [
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1
+        ]
+    }
+];
+  module.exports = friends;
