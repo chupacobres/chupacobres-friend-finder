9024109e17a88b3f04790d69c51dfee6d55de367
juan uribe <juanpablouribe@mac.com>
2019-12-14 11:58:11 -0500

    fixed a bug on database friends that wanted to show a picture

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,7 +2,6 @@
 var friends = [
     {
         "name": "Papi",
-        "photo": "https://am11.akamaized.net/med/cnt/uploads/2010/01/Screen-shot-2010-01-31-at-11.47.04-AM-e1264957975704.png",
         "scores": [
             5,
             1,
@@ -18,7 +17,6 @@ var friends = [
     },
     {
         "name": "Donald Trump",
-        "photo": "http://baskino.me/uploads/images/2013/104/oqnx12.jpg",
         "scores": [
             5,
             5,
@@ -34,7 +32,6 @@ var friends = [
     },
     {
         "name": "Cata Lina",
-        "photo": "http://images.tmz.com/2016/09/15/0915-hatchet-face-kim-macguire-cry-baby-5.jpg",
         "scores": [
             5,
             5,
@@ -50,7 +47,6 @@ var friends = [
     },
     {
         "name": "Satan",
-        "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBCzJN2jYwe6IDoShgz21y5fvOVg-EbvQTfM1VzKnv6QvejX-E",
         "scores": [
             3,
             5,
@@ -66,7 +62,6 @@ var friends = [
     },
     {
         "name": "Lovee Mi",
-        "photo": "https://ace_hotel_dev.s3.amazonaws.com/images/queen_of_the_damned.jpg",
         "scores": [
             1,
             1,
